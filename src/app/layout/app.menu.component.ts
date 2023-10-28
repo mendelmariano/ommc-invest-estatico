d48774943b03ae265cc47bb1936c685a45527c0b
LcsfCarlos <luiz182013@gmail.com>
2023-10-28 18:19:35 -0300

    Iniciando os trabalhos

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -26,34 +26,6 @@ export class AppMenuComponent implements OnInit {
                         ]
                     },
 
-
-                    {
-                        label: 'Jogar',
-                        items: [
-                            { label: 'Criar partida', icon: 'pi pi-fw pi-play', routerLink: ['/results/create'] },
-                            { label: 'Meus jogos', icon: 'pi pi-fw pi-database', routerLink: ['/results/myList'] },
-
-                        ]
-                    },
-
-                    {
-                        label: 'Times',
-                        items: [
-                            { label: 'Criar time', icon: 'pi pi-fw pi-angle-double-down', routerLink: ['/teams/create'] },
-                            { label: 'Meus times', icon: 'pi pi-fw pi-users', routerLink: ['/teams/myList'] },
-
-                        ]
-                    },
-
-                    {
-                        label: 'Campeonatos',
-                        items: [
-                            { label: 'Lista de Campeonatos', icon: 'pi pi-fw pi-server', routerLink: ['/championships/list'] },
-                            { label: 'Meus Campeonatos', icon: 'pi pi-fw pi-verified', routerLink: ['/championships/myList'] },
-                            { label: 'Classificação', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/championships/classifications'] },
-                        ]
-                    },
-
                 ];
                 if(user.profile_id === 2){
                     this.model.push(
@@ -61,24 +33,6 @@ export class AppMenuComponent implements OnInit {
                             label: 'Administrador',
                             items: [
                                 { label: 'Usuários', icon: 'pi pi-fw pi-user', routerLink: ['/administrator/'] },
-                                {
-                                    label: 'Campeonatos',
-                                    icon: 'pi pi-fw pi-verified',
-                                    items: [
-                                        {
-                                            label: 'Criar',
-                                            icon: 'pi pi-fw pi-sign-in',
-                                            routerLink: ['/administrator/championship']
-                                        },
-                                        {
-                                            label: 'Liberar participantes',
-                                            icon: 'pi pi-fw pi-check',
-                                            routerLink: ['/administrator/championship/check']
-                                        },
-                                    ]
-                                },
-                                { label: 'Modos de Jogo', icon: 'pi pi-fw pi-server', routerLink: ['/administrator/mode'] },
-                                { label: 'Liberar times', icon: 'pi pi-fw pi-check', routerLink: ['/administrator/team/check'] },
                             ]
                         },
                     )
